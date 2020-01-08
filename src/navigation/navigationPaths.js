a3d0f38402f606b0472aa141edfcdfb1c42d1ec6
Robin Vitre <robin.vitre@gmail.com>
2020-01-08 09:47:56 +0100

    remove old

--- src/navigation/navigationPaths.js
+++ src/navigation/navigationPaths.js
@@ -3,6 +3,4 @@ export const LOGIN = "LOGIN";
 
 
 export const LOGGED_IN = "LOGGED_IN"
-export const CYBER_HOME = "CYBER_HOME";
-export const VPN_HOME = "VPN_HOME";
 export const PORTFOLIO_HOME = "PORTFOLIO_HOME"
--- src/navigation/vpn/navigationPaths.js
+++
@@ -1,9 +0,0 @@
-const VPN = 'VPN';
-
-const HOME = `${VPN}/home`;
-const SERVER_LIST = `${VPN}/server-list`;
-
-export {
-  HOME,
-  SERVER_LIST
-};
